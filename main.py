3415ecda982da39f40f7aef1d2a4bc0422a77030
QuentinLTRD <quentin.leturdu@gmail.com>
2023-10-07 12:18:02 +0200

    add: first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from board import Board
+from move import play_move
+
+if __name__ == "__main__":
+    board = Board()
+    print(board)
+
+    move="e2"
+    print(move[0])
+    print(move[-1])
+
+    fen="d4f4e7 / a2a8 / e4 e6 / 7 8 / 2"
+    print(play_move(fen, "e2"))
+    
+    board = Board(fen=fen)
+    print(board)
+    while True:
+        move = input("Move ?")
+        fen = play_move(fen, move)
+        board = Board(fen=fen)
+        print(board)
+
+
+
---
+++ move.py
@@ -0,0 +1,19 @@
+def play_move(fen, move):
+    hwalls, vwalls, player_positions, available_walls, ply  = fen.split(" / ")
+    if move[-1] == "h":
+        fen = f"{hwalls}{move[:2]} / {vwalls} / {player_positions} / {available_walls} / {ply}"
+
+    elif move[-1] == "v":
+        fen = f"{hwalls} / {vwalls}{move[:2]} / {player_positions} / {available_walls} / {ply}"
+    
+    else:
+        p1, p2 = player_positions.split(" ")
+        if ply == 1:
+            fen = f"{hwalls} / {vwalls} / {move} {p2} / {available_walls} / {(int(ply) + 1)}"
+        
+        else:
+            fen = f"{hwalls} / {vwalls} / {p1} {move} / {available_walls} / {(int(ply) - 1)}"
+    
+    return fen
+
+
