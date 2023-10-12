08f8393b9c3936d62b3e8b18d768a0029ea7d5e4
Arthur-Vat <vattier.arthur@gmail.com>
2023-10-12 08:20:46 +0200

    add a check for pawn move generation

--- utils.py
+++ utils.py
@@ -1,19 +1,18 @@
 from string import ascii_lowercase
+from constants import BOARD_SIZE
 
 
 def not2pos(pos_notation):
-    # gets pos indices from pos notation (ex: a1 -> 0, 0; d2 -> 1, 3)
+    # gets pos indices from pos notation (ex: a1 -> (0, 0) ; d2 -> (1, 3))
     col = ord(pos_notation[0]) - 97  # convert lowercase to index
     row = int(pos_notation[1]) - 1  # rows index number is 1
     return row, col
-
 
 def pos2not(pos):
     # get pos notation from pos indices (ex: (0, 0) -> a1 ; (1, 3) -> d2)
     pos_row = int(pos[0]) + 1
     pos_col = ascii_lowercase[pos[1]]
     return f"{pos_col}{pos_row}"
-
 
 def play_move(fen, move):
     # updates a fen given a move (wikipedia notations)
@@ -41,3 +40,6 @@ def play_move(fen, move):
     fen = f"{hwalls} / {vwalls} / {pawn_positions} / {fences_left} / {active_player}"
 
     return fen
+
+def is_within_bounds(pos):
+    return (0 <= pos[0] < BOARD_SIZE) and (0 <= pos[1] < BOARD_SIZE)
